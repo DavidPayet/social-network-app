e16efb9517cdea5af83f76024d891c2766692e16
DavidPayet <davidpayet570@gmail.com>
2020-03-11 12:18:21 +0100

    add google login btn

--- src/pages/ConnectionPage.jsx
+++ src/pages/ConnectionPage.jsx
@@ -34,11 +34,6 @@ const ConnectionPage = () => {
             <FaFacebook className="login-icons" /> Facebook
           </button>
         </Row>
-        {/* <Row>
-          <button className="login-btn">
-            <FaGoogle className="login-icons" /> Google
-          </button>
-        </Row> */}
         <Row>
           <button
             className="login-btn"
@@ -46,6 +41,11 @@ const ConnectionPage = () => {
             type="submit"
           >
             <FaTwitter className="login-icons" /> Twitter
+          </button>
+        </Row>
+        <Row>
+          <button className="login-btn">
+            <FaGoogle className="login-icons" /> Google
           </button>
         </Row>
       </Container>
